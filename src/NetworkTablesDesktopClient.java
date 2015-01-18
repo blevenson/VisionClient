a4ec32a4ec6f3f0a381214e9130fce02d9b21883
blevenson <blevenson68@gmail.com>
2015-01-18 13:40:24 -0800

    Added filters to show outline

--- src/NetworkTablesDesktopClient.java
+++ src/NetworkTablesDesktopClient.java
@@ -3,6 +3,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -10,7 +11,10 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 
@@ -32,7 +36,7 @@ public class NetworkTablesDesktopClient {
 
 	public void run() {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		VideoCapture vid = new VideoCapture(0);
+		VideoCapture vid = new VideoCapture(1);
 		
 		NetworkTable.setClientMode();
 		NetworkTable.setIPAddress("roborio-766.local");
@@ -49,17 +53,19 @@ public class NetworkTablesDesktopClient {
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		mainPanel.add(lblimage);
 		frame.add(mainPanel);
+		frame.setSize(500, 500);
 		frame.setVisible(true);
 		
 		Mat pic = new Mat();
-
+		
 		/*
 		 * Done starts as true, so that if the image processing here does not
 		 * work, or is not called, the commands during auto don't get hungup.
 		 */
 		table.putBoolean("done", false);
 		boolean done = (table.getBoolean("done"));
-
+		
+		Mat hsv = new Mat();
 		Mat satImg = new Mat();
 		while (!done) {
 //			try {
@@ -72,15 +78,24 @@ public class NetworkTablesDesktopClient {
 			// update done
 			vid.read(img);
 			//Imgproc.threshold(img, pic, 100, 255, Imgproc.THRESH_BINARY);
-			Imgproc.cvtColor(img, pic,  Imgproc.COLOR_RGB2HSV);
-			ArrayList<Mat> channels = new ArrayList<Mat>();
-			Core.split(pic, channels);
-			satImg = channels.get(1);
-			Imgproc.medianBlur(satImg , satImg , 11);
-			Imgproc.adaptiveThreshold(satImg , satImg , 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 401, -10);
-			lblimage.setIcon(new ImageIcon(toBufferedImage(satImg)));
-
-			table.putNumber("leftMotor", leftMotorC);
+//			Imgproc.cvtColor(img, pic,  Imgproc.COLOR_RGB2HSV);
+//			ArrayList<Mat> channels = new ArrayList<Mat>();
+//			Core.split(pic, channels);
+//			satImg = channels.get(1);
+//			Imgproc.medianBlur(satImg , satImg , 11);
+//			Imgproc.adaptiveThreshold(satImg , satImg , 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 401, -10);
+			
+		    Imgproc.cvtColor(img, hsv, Imgproc.COLOR_RGB2HSV);
+		    //Imgproc.threshold(hsv, satImg, 250, 800, Imgproc.THRESH_BINARY);
+		    Core.inRange(hsv, new Scalar(49, 100, 100), new Scalar(55,255,255), satImg);
+		    
+		     
+		    
+		    lblimage.setIcon(new ImageIcon(toBufferedImage(satImg)));
+		    
+		    
+		    
+		    table.putNumber("leftMotor", leftMotorC);
 			table.putNumber("rightMotor", rightMotorC);
 			//System.out.println("Left: " + leftMotorC + " Right: " + rightMotorC);
 
